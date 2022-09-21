c5a393129b73860fb1647c964037f3e826a5af03
David Bulko <david.bulko1986@gmail.com>
2022-09-21 08:18:21 +0200

    first commit

---
+++ src/main/java/sk/sosholic/prvypolrok/Opakovanie.java
@@ -0,0 +1,9 @@
+package sk.sosholic.prvypolrok;
+
+public class Opakovanie {
+    public static void main(String[] args) {
+
+        System.out.println("Poloval sem");
+        System.out.println("Hledal sem prax");
+    }
+}
---
+++ src/main/java/sk/sosholic/prvypolrok/datoveTypy.java
@@ -0,0 +1,23 @@
+package sk.sosholic.prvypolrok;
+
+public class datoveTypy {
+    public static void main(String[] args) {
+        int x =1;
+        byte f =8;
+        short c = 17;
+        long a = 64;
+        float k =32;
+        double l = 64;
+        char p = 10;
+        boolean O = true;
+
+        System.out.println(x);
+        System.out.println(f);
+        System.out.println(c);
+        System.out.println(a);
+        System.out.println(k);
+        System.out.println(l);
+        System.out.println(p);
+        System.out.println(O);
+    }
+}
